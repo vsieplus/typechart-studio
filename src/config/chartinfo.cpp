c12618c8e76ecfe3bd4250334b67d1227be33104
rsie <ryansie@hotmail.com>
2022-04-20 23:46:37 -0700

    notes for chartinfo json

--- src/config/chartinfo.cpp
+++ src/config/chartinfo.cpp
@@ -4,6 +4,10 @@
 
 using json = nlohmann::json;
 
+// write prettified JSON to another file
+//std::ofstream o("pretty.json");
+//o << std::setw(4) << j << std::endl;
+
 ChartInfo::ChartInfo(std::string chartPath) {
 
 }
@@ -12,5 +16,5 @@ ChartInfo::ChartInfo(int level, std::string typist, std::string keyboardLayout) 
         level(level), typist(typist), keyboardLayout(keyboardLayout) {}
 
 void ChartInfo::saveChart(std::string chartPath) {
-    
+
 }
