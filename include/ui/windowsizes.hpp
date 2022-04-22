713c60a53f866db80b8e69eacae31d3096b7b187
rsie <ryansie@hotmail.com>
2022-04-21 17:17:55 -0700

    update window sizes

--- include/ui/windowsizes.hpp
+++ include/ui/windowsizes.hpp
@@ -7,8 +7,8 @@ const int SCREEN_WIDTH = 1280;
 const int SCREEN_HEIGHT = 720;
 
 // default window sizes
-inline ImVec2 maxFDSize = ImVec2(SCREEN_WIDTH, SCREEN_HEIGHT);
-inline ImVec2 minFDSize = ImVec2(SCREEN_WIDTH*3.f/4, SCREEN_HEIGHT*3.f/4);
+inline ImVec2 maxFDSize = ImVec2(SCREEN_WIDTH*3.f/4, SCREEN_HEIGHT*3.f/4);
+inline ImVec2 minFDSize = ImVec2(SCREEN_WIDTH/2.f, SCREEN_HEIGHT/2.f);
 inline ImVec2 newEditWindowSize = ImVec2(500, 450);
 
 #endif
