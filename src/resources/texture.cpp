8b946b67afd9bc87fc8e177fc53e7b4c9b6da95d
rsie <ryansie@hotmail.com>
2022-04-23 08:35:31 -0700

    fix loadTexture namespace

--- src/resources/texture.cpp
+++ src/resources/texture.cpp
@@ -2,20 +2,22 @@
 
 #include <SDL2/SDL_image.h>
 
-std::shared_ptr<SDL_Texture> loadTexture(std::string path, SDL_Renderer * renderer) {
-    SDL_Surface * surface = IMG_Load(path.c_str());
-    if(!surface) {
-        return nullptr;
+namespace Texture {
+    std::shared_ptr<SDL_Texture> loadTexture(std::string path, SDL_Renderer * renderer) {
+        SDL_Surface * surface = IMG_Load(path.c_str());
+        if(!surface) {
+            return nullptr;
+        }
+
+        // Create SDL texture from the surface
+        std::shared_ptr<SDL_Texture> newTexture (SDL_CreateTextureFromSurface(renderer, surface), SDL_DestroyTexture);
+            
+        if(!newTexture.get()) {
+            return nullptr;
+        }
+
+        SDL_FreeSurface(surface);
+
+        return newTexture;
     }
-
-    // Create SDL texture from the surface
-    std::shared_ptr<SDL_Texture> newTexture (SDL_CreateTextureFromSurface(renderer, surface), SDL_DestroyTexture);
-        
-    if(!newTexture.get()) {
-        return nullptr;
-    }
-
-    SDL_FreeSurface(surface);
-
-    return newTexture;
 }
