2acefeba7d04f53a51a4cdf00a59e02cdd800933
rsie <ryansie@hotmail.com>
2022-04-28 22:22:04 -0700

    generalize notesequenceitem, fix get() function

--- include/config/notesequence.hpp
+++ include/config/notesequence.hpp
@@ -2,11 +2,17 @@
 #define NOTESEQUENCE_HPP
 
 #include <algorithm>
+#include <list>
+#include <vector>
 
 #include "imgui.h"
 #define IMGUI_DEFINE_MATH_OPERATORS
 #include "imgui_internal.h"
 #include "ImSequencer.h"
+
+#include "config/note.hpp"
+#include "config/skip.hpp"
+#include "config/stop.hpp"
 
 static const char* SequencerItemTypeNames[] = { "Lane 1 [Top]", "Lane 2 [Middle]", "Lane 3 [Bottom]", "Stops", "Skips" };
 
@@ -14,25 +20,74 @@ struct NoteSequence : public ImSequencer::SequenceInterface {
     // my datas
     NoteSequence() : mFrameMin(0.f), mFrameMax(1000.f) {}
 
-    enum SequencerItemType {
-        TOP_NOTE,
-        MID_NOTE,
-        BOT_NOTE,
-        STOP,
-        SKIP,
-        NUM_ITEMTYPES
-    };
-
-    int numLanes = 5;
-
     float mFrameMin, mFrameMax;
-    struct NoteSequenceItem
-    {
-        SequencerItemType mType;
-        float mFrameStart, mFrameEnd;
-    };
 
-    std::vector<NoteSequenceItem> myItems;
+    std::list<Note> mNotes;
+    std::list<Stop> mStops;
+    std::list<Stop> mSkips;
+
+    std::vector<NoteSequenceItem *> myItems;
+
+    void addNote(float absBeat, float beatDuration, SequencerItemType itemType) {
+        auto iter = mNotes.begin();
+        while(iter != mNotes.end()) {
+            auto & note = *iter;
+
+            if(absBeat < note.absBeat) {
+                break;
+            }
+
+            iter++;
+        }
+
+        Note newNote = Note(absBeat, absBeat + beatDuration, NoteType::KEYPRESS, NoteSplit::EIGHTH, itemType, "A");
+
+        if(iter == mNotes.end()) {
+            mNotes.push_back(newNote);
+            myItems.push_back(&mNotes.back());
+        } else {
+            iter = mNotes.insert(iter, newNote);
+            myItems.push_back(&(*iter));
+        }
+    }
+
+    void addStop(float absBeat, float beatDuration) {
+
+    }
+
+    void addSkip(float absBeat, float beatDuration) {
+
+    }
+
+    void addItem(float absBeat, float beatDuration, int itemType) {
+        switch(itemType) {
+            case SequencerItemType::TOP_NOTE:
+            case SequencerItemType::MID_NOTE:
+            case SequencerItemType::BOT_NOTE:
+                addNote(absBeat, beatDuration, (SequencerItemType)itemType);
+                break;
+            case SequencerItemType::STOP:
+                addStop(absBeat, beatDuration);
+                break;
+            case SequencerItemType::SKIP:
+                addSkip(absBeat, beatDuration);
+                break;
+        }
+    }
+
+    void editItem(float absBeat, int itemType) {
+
+    }
+
+    bool containsItemAt(float absBeat, int itemType) {
+        for(auto seqItem : myItems) {
+            if((int)(seqItem->itemType) == itemType && absBeat >= seqItem->absBeat && absBeat < seqItem->beatEnd) {
+                return true;
+            } 
+        }
+
+        return false;
+    }
 
     virtual int GetFrameMin() const {
         return mFrameMin;
@@ -40,32 +95,25 @@ struct NoteSequence : public ImSequencer::SequenceInterface {
     virtual int GetFrameMax() const {
         return mFrameMax;
     }
+
     virtual int GetItemCount() const { return myItems.size(); }
 
     virtual int GetItemTypeCount() const { return sizeof(SequencerItemTypeNames) / sizeof(char*); }
     virtual const char* GetItemTypeName(int typeIndex) const { return SequencerItemTypeNames[typeIndex]; }
-    virtual const char* GetItemLabel(int index) const
-    {
-        static char tmps[512];
-        snprintf(tmps, 512, "%s", SequencerItemTypeNames[myItems[index].mType]);
-        return tmps;
-    }
+    virtual const char* GetItemLabel(int index) const { return ""; }
 
     virtual void Get(int index, float** start, float** end, int* type, unsigned int* color)
     {
-        NoteSequenceItem& item = myItems[index];
+        NoteSequenceItem * item = myItems[index];
         if (color)
             *color = 0xFFAA8080; // same color for everyone, return color based on type
         if (start)
-            *start = &item.mFrameStart;
+            *start = &(item->absBeat);
         if (end)
-            *end = &item.mFrameEnd;
-        if (type)
-            *type = item.mType;
+            *end = &(item->beatEnd);
+        if(type)
+            *type = (int)(item->itemType);
     }
-    virtual void Add(int type) { myItems.push_back(NoteSequenceItem{ (SequencerItemType)type, 0, 10 }); };
-    virtual void Del(int index) { myItems.erase(myItems.begin() + index); }
-    virtual void Duplicate(int index) { myItems.push_back(myItems[index]); }
 
     virtual size_t GetCustomHeight(int index) { return 30; }
 };
