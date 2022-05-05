19c9da02ac74e99426a7f384b885b6427bf029d4
rsie <ryansie@hotmail.com>
2022-05-04 23:37:11 -0700

    first implementation of chart loading

--- src/config/chartinfo.cpp
+++ src/config/chartinfo.cpp
@@ -7,6 +7,29 @@
 using json = nlohmann::json;
 using ordered_json = nlohmann::ordered_json;
 
+float calculateAbsBeat(BeatPos beatpos, std::vector<Timeinfo> & timeinfo) {
+    float absBeat = 0.f;
+    int prevSectionBeatsPerMeasure = 4;
+    BeatPos prevSectionBeatpos = {0, 1, 0};
+    
+    for(auto & section: timeinfo) {
+        if(beatpos < section.beatpos || section.absBeatStart == timeinfo.back().absBeatStart) {
+            float prevSectionMeasure = prevSectionBeatpos.measure + (prevSectionBeatpos.split / (float)prevSectionBeatpos.beatsplit);
+            float currSectionMeasure = beatpos.measure + (beatpos.split / (float)beatpos.beatsplit);
+
+            float currSectionBeats = (currSectionMeasure - prevSectionMeasure) * prevSectionBeatsPerMeasure;
+            absBeat += currSectionBeats;
+            break;
+        } else {
+            absBeat = section.absBeatStart;
+            prevSectionBeatpos = section.beatpos;
+            prevSectionBeatsPerMeasure = section.beatsPerMeasure;
+        }
+    }
+
+    return absBeat;
+}
+
 ChartInfo::ChartInfo() {}
 
 ChartInfo::ChartInfo(int level, std::string typist, std::string keyboardLayout) : 
@@ -35,11 +58,98 @@ bool ChartInfo::loadChart(std::string chartPath, SongPosition & songpos) {
             int beatsPerMeasure = sectionJSON["beatsPerMeasure"];
 
             songpos.timeinfo.push_back(Timeinfo((BeatPos){pos.at(0), pos.at(1), pos.at(2)}, prevTimeinfo, beatsPerMeasure, bpm));
+            prevTimeinfo = &(songpos.timeinfo.back());
         }
 
+        std::sort(songpos.timeinfo.begin(), songpos.timeinfo.end());
+
+        std::vector<ordered_json> stopsJSON = chartinfoJSON["stops"];
+        for(auto & stopJSON : stopsJSON) {
+            std::vector<int> pos = stopJSON["pos"];
+            float beatDuration = stopJSON["duration"];
+
+            //notes.addStop()
+        }
+
+        std::vector<ordered_json> skipsJSON = chartinfoJSON["skips"];
+        for(auto & skipJSON : skipsJSON) {
+            std::vector<int> pos = skipJSON["pos"];
+            float beatDuration = skipJSON["duration"];
+            float skipTime = skipJSON["skiptime"];
+
+            //notes.addSkip()
+        }
+
+        std::vector<ordered_json> notesJSON = chartinfoJSON["notes"];
+        for(auto iter = notesJSON.begin(); iter != notesJSON.end(); iter++) {
+            auto & noteJSON = *iter;
+
+            NoteType noteType = (NoteType)(noteJSON["type"].get<int>());
+            
+            // release notes handled separately below
+            if(noteType == NoteType::KEYHOLDRELEASE) {
+                continue;
+            }
+
+            std::vector<int> pos = noteJSON["pos"];
+            if(pos.size() != 3) {
+                continue;
+            }
+
+            BeatPos beatpos = (BeatPos){pos.at(0), pos.at(1), pos.at(2)};
+            BeatPos endBeatpos = beatpos;
+            std::string keyText = noteJSON["key"];
+
+            switch(noteType) {
+                case NoteType::KEYPRESS:
+                    break;
+                case NoteType::KEYHOLDSTART:
+                    // find next matching release note
+                    for(auto nIter = std::next(iter); nIter != notesJSON.end(); nIter++) {
+                        auto & nextNoteJSON = *nIter;
+
+                        NoteType nextNoteType = (NoteType)(nextNoteJSON["type"].get<int>());
+                        std::string nextNoteKeyText = nextNoteJSON["key"];
+
+                        if(nextNoteKeyText == keyText && nextNoteType == NoteType::KEYHOLDRELEASE) {
+                            std::vector<int> nextNotepos = nextNoteJSON["pos"];
+                            endBeatpos = (BeatPos) { nextNotepos.at(0), nextNotepos.at(1), nextNotepos.at(2) };
+                            break;
+                        }
+                    }
+
+                    break;
+                case NoteType::KEYHOLDRELEASE:
+                    continue;                    
+            }
+
+            SequencerItemType itemType;
+            if(keyText.length() > 1) {
+                itemType = SequencerItemType::BOT_NOTE;
+            } else if(MIDDLE_ROW_KEYS.find(keyboardLayout) != MIDDLE_ROW_KEYS.end()) {
+                auto & validKeys = MIDDLE_ROW_KEYS.at(keyboardLayout);
+                if(validKeys.find(keyText.at(0)) != validKeys.end()) {
+                    itemType = SequencerItemType::MID_NOTE;
+                } else {
+                    itemType = SequencerItemType::TOP_NOTE;
+                }
+            } else {
+                // unsupported keyboard layout
+                return false;
+            }
+
+            float absBeat = calculateAbsBeat(beatpos, songpos.timeinfo);
+            float absBeatEnd = calculateAbsBeat(endBeatpos, songpos.timeinfo);
+
+            float beatDuration = absBeatEnd - absBeat;
+
+            notes.addNote(absBeat, beatDuration, beatpos, endBeatpos, itemType, keyText);
+        }
     } catch(...) {
         return false;
     }
+
+    std::sort(notes.myItems.begin(), notes.myItems.end());
 
     return true;
 }
@@ -67,9 +177,9 @@ void ChartInfo::saveChart(std::string chartPath, SongPosition & songpos) {
 
     chartinfo["timeinfo"] = timeinfo;
 
-    ordered_json stopsJSON;
-    ordered_json skipsJSON;
-    ordered_json notesJSON;
+    ordered_json stopsJSON = ordered_json::array();
+    ordered_json skipsJSON = ordered_json::array();
+    ordered_json notesJSON = ordered_json::array();
 
     for(auto & item : notes.myItems) {
         ordered_json itemJSON;
