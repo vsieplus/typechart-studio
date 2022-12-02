2957a91c820ebd016b0a4af68072332c3fbb9e82
rsie <ryansie@hotmail.com>
2022-12-02 09:39:57 -0800

    update new chart window size

--- include/ui/windowsizes.hpp
+++ include/ui/windowsizes.hpp
@@ -9,6 +9,6 @@ const int SCREEN_HEIGHT = 1080;
 // default window sizes
 inline ImVec2 maxFDSize = ImVec2(SCREEN_WIDTH*3.f/4, SCREEN_HEIGHT*3.f/4);
 inline ImVec2 minFDSize = ImVec2(SCREEN_WIDTH/2.f, SCREEN_HEIGHT/2.f);
-inline ImVec2 newEditWindowSize = ImVec2(500, 450);
+inline ImVec2 newEditWindowSize = ImVec2(500, 500);
 
 #endif
